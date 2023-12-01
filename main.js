6512a79a4c640bed80edd6ad7c9ee86bc50b74f5
Vaibhav Thakare <98647776+VaibhavThakare07@users.noreply.github.com>
2023-12-01 15:32:21 +0530

    commit

---
+++ main.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".text", {
+  strings: ["Full Stack Developer", "Freelancer", "Web Developer"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  startDelay: 1000,
+  backDelay: 1000,
+  loop: true,
+});
